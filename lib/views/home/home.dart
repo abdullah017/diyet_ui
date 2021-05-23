c89985e22486b2642c3fe92b8fac4f8ae735f5ec
Abdullah Taş <dev.abdullahtas@gmail.com>
2021-05-23 23:12:50 +0300

    ana sayfa -home- responsive yapıldı

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -14,116 +14,112 @@ class HomeView extends StatelessWidget {
       home: Scaffold(
         appBar: HomeAppBar(),
         drawer: Drawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Stack(
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      "HOŞGELDİN HASAN\nBUGÜN NASILSIN?",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "HOŞGELDİN HASAN\nBUGÜN NASILSIN?",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
                   ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    color: Color(0xff654769),
-                    elevation: 10,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          const ListTile(
-                            leading: Icon(
-                              Icons.access_time_rounded,
-                              size: 50,
-                              color: Colors.white,
-                            ),
-                            title: Text('Randevu',
-                                style: TextStyle(color: Colors.white)),
-                            subtitle: Text(
-                              'Yakınlarda bir randevunuz bulunmamaktadır.',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Container(
-                      width: displayWidth(context),
-                      height: displayHeight(context) * 0.2,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Color(0xff69C7B7),
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                color: Color(0xff654769),
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(
+                          Icons.access_time_rounded,
+                          size: 50,
+                          color: Colors.white,
                         ),
-                        color: Color(0xff69C7B7),
-                        //borderRadius: BorderRadius.circular(100),
+                        title: Text('Randevu',
+                            style: TextStyle(color: Colors.white)),
+                        subtitle: Text(
+                          'Yakınlarda bir randevunuz bulunmamaktadır.',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
-                      child: Center(
-                        child: TextButton(
-                          onPressed: () {
-                            print('BANA BASTINmmasdfasd');
-                          },
-                          child: Text(
-                            "Kategorini\nSeç\nHemen Başla",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                          ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Container(
+                  width: displayWidth(context),
+                  height: displayHeight(context) * 0.2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shape: BoxShape.rectangle,
+                    border: Border.all(
+                      color: Color(0xff69C7B7),
+                    ),
+                    color: Color(0xff69C7B7),
+                    //borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () {
+                        print('BANA BASTINmmasdfasd');
+                      },
+                      child: Text(
+                        "Kategorini Seç\nHemen Başla",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(height: 15),
-                  Expanded(
-                    flex: 0,
-                    child: Container(
-                      width: double.infinity,
-                      height: 200.0,
-                      child: ListView.builder(
-                        itemCount: doctorList.length,
-                        scrollDirection: Axis.horizontal,
-                        physics: ScrollPhysics(),
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          var doctor = doctorList[index];
+                ),
+              ),
+              SizedBox(height: 25),
+              Expanded(
+                flex: 0,
+                child: Container(
+                  width: double.infinity,
+                  height: 200.0,
+                  child: ListView.builder(
+                    itemCount: doctorList.length,
+                    scrollDirection: Axis.horizontal,
+                    physics: ScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      var doctor = doctorList[index];
 
-                          return GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      DiyetisyenListViewDetail(
-                                    doctorModel: doctorList[index],
-                                  ),
-                                ),
-                              );
-                            },
-                            child: DoctorCard(doctor: doctor),
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => DiyetisyenListViewDetail(
+                                doctorModel: doctorList[index],
+                              ),
+                            ),
                           );
                         },
-                      ),
-                    ),
+                        child: DoctorCard(doctor: doctor),
+                      );
+                    },
                   ),
-                ],
+                ),
               ),
             ],
           ),
